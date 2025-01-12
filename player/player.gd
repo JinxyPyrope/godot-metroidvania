601602e2595381eba100470df55cbb3f58d665e8
JinxyPyrope <jinxypy@yahoo.com>
2025-01-12 11:18:16 -0500

    Cretae player idle state

---
+++ player/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+const GRAVITY = 1000
+
+enum State {Idle, Run}
+
+var current_state
+
+func _ready():
+	current_state = State.Idle
+	
+func _physics_process(delta):
+	player_falling(delta)
+	player_idle(delta)
+	move_and_slide()
+	
+func player_falling(delta):
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+
+func player_idle(delta):
+	if is_on_floor():
+		current_state = State.Idle
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+#
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
