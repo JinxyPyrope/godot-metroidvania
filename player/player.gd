b608a90ee0792c0df419c9a853225245270d04a6
JinxyPyrope <jinxypy@yahoo.com>
2025-01-12 11:33:12 -0500

    Basic player movement and gravity add in

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
+const SPEED = 300
 
 enum State {Idle, Run}
 
@@ -12,7 +14,9 @@ func _ready():
 func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
+	player_run(delta)
 	move_and_slide()
+	player_animations()
 	
 func player_falling(delta):
 	if !is_on_floor():
@@ -22,6 +26,24 @@ func player_idle(delta):
 	if is_on_floor():
 		current_state = State.Idle
 
+func player_run(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction != 0:
+		current_state = State.Run
+		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+func player_animations():
+	if current_state == State.Idle:
+		animated_sprite_2d.play('idle')
+	elif current_state == State.Run:
+		animated_sprite_2d.play('run')
+		
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 #
