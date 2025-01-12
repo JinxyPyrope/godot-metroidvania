3509dbb93e4c8910d287f02548542e1fd63d7889
JinxyPyrope <jinxypy@yahoo.com>
2025-01-12 12:28:45 -0500

    Player jump state added

--- player/player.gd
+++ player/player.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 
 const GRAVITY = 1000
 const SPEED = 300
+const JUMP = -200
+const JUMP_HORIZONTAL = 100 
 
-enum State {Idle, Run}
+enum State {Idle, Run, Jump }
 
 var current_state
+
+
 
 func _ready():
 	current_state = State.Idle
@@ -15,8 +19,11 @@ func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
+	player_jump(delta)
 	move_and_slide()
 	player_animations()
+	
+	print("State ", State.keys()[current_state])
 	
 func player_falling(delta):
 	if !is_on_floor():
@@ -25,9 +32,12 @@ func player_falling(delta):
 func player_idle(delta):
 	if is_on_floor():
 		current_state = State.Idle
-		print("State ", State.keys()[current_state])
+		
 
 func player_run(delta):
+	if !is_on_floor():
+		return
+		
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction:
@@ -38,34 +48,21 @@ func player_run(delta):
 	if direction != 0:
 		current_state = State.Run
 		animated_sprite_2d.flip_h = false if direction > 0 else true
-	print("State ", State.keys()[current_state])
 	
 	
+func player_jump(delta):
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP
+		current_state = State.Jump
+	
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = Input.get_axis("move_left", "move_right")
+		velocity.x += direction * JUMP_HORIZONTAL * delta
+
 func player_animations():
 	if current_state == State.Idle:
 		animated_sprite_2d.play('idle')
 	elif current_state == State.Run:
 		animated_sprite_2d.play('run')
-		
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-#
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
+	elif current_state == State.Jump:
+		animated_sprite_2d.play('jump')
