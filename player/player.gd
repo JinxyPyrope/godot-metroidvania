46617a9cfacdc6eb38c353fbd77787f762f714bd
JinxyPyrope <jinxypy@yahoo.com>
2025-01-13 09:05:23 -0500

    Adding delta to character fr better control and for different optimizations

--- player/player.gd
+++ player/player.gd
@@ -4,8 +4,13 @@ extends CharacterBody2D
 const GRAVITY = 1000
 const SPEED = 300
 
-@export var speed : int = 300
+@export var speed : int = 1000
+@export var max_horizontal_speed: int = 300
+@export var slow_down_speed : int = 1700
+
 @export var jump : int = -300
+@export var jump_horizontal_speed: int = 1000
+@export var max_jump_horizontal_speed: int = 300
 @export var jump_horizontal : int = 100
 
 enum State {Idle, Run, Jump }
@@ -46,9 +51,10 @@ func player_run(_delta : float):
 	var direction = input_movement()
 	
 	if direction:
-		velocity.x = direction * speed
+		velocity.x += direction * speed * _delta
+		velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, slow_down_speed * _delta)
 	
 	if direction != 0:
 		current_state = State.Run
@@ -62,7 +68,8 @@ func player_jump(delta : float):
 	
 	if !is_on_floor() and current_state == State.Jump:
 		var direction = input_movement()
-		velocity.x += direction * jump_horizontal * delta
+		velocity.x += direction * jump_horizontal_speed * delta
+		velocity.x = clamp(velocity.x, -max_jump_horizontal_speed, max_jump_horizontal_speed)
 
 func player_animations():
 	if current_state == State.Idle:
