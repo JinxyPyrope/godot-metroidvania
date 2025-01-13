9c9b50918ca98e4da50dc7c5c0555d863ff94633
JinxyPyrope <jinxypy@yahoo.com>
2025-01-13 15:55:30 -0500

    Add bullet efect then move onto adding bullet damage

--- player/bullet.gd
+++ player/bullet.gd
@@ -1,7 +1,10 @@
 extends AnimatedSprite2D
 
+var bullet_impact_effect = preload("res://bullet_impact_effect.tscn")
+
 var speed : int = 400
 var direction : int
+
 
 func _physics_process(delta):
 	move_local_x(direction * speed * delta)
@@ -13,7 +16,14 @@ func _on_timer_timeout() -> void:
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	print("Bullet Area Entered")
+	bullet_impact()
 
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	print("Bullet body entered")
+
+func bullet_impact():
+	var bullet_impact_effect_instance = bullet_impact_effect.instantiate() as Node2D
+	bullet_impact_effect_instance.global_position = global_position
+	get_parent().add_child(bullet_impact_effect_instance)
+	queue_free()
