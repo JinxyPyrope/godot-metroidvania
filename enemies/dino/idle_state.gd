7a9a21eed70b2c45185769b51b4324b5839fcf31
JinxyPyrope <jinxypy@yahoo.com>
2025-01-14 10:58:04 -0500

    Create attack state

---
+++ enemies/dino/idle_state.gd
@@ -0,0 +1,19 @@
+extends NodeState
+
+
+@export var character_body_2d : CharacterBody2D
+@export var charcater_animated_2d : AnimatedSprite2D
+@export var slow_down_speed : int = 50 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float):
+	pass
+	
+func enter():
+	pass
+	
+func exit():
+	pass
