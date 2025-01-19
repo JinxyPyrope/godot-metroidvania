ec02620091f088b9bd3884dc27c7031d2c1f20f1
JinxyPyrope <jinxypy@yahoo.com>
2025-01-19 13:45:32 -0500

    Give the player the death effect

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var bullet = preload("res://player/bullet.tscn")
-
+var player_death_effect = preload("res://player/player_death_effect/player_death_effect.tscn")
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var muzzle : Marker2D = $Muzzle
@@ -81,7 +81,7 @@ func player_jump(delta : float):
 		velocity.x += direction * jump_horizontal_speed * delta
 		velocity.x = clamp(velocity.x, -max_jump_horizontal_speed, max_jump_horizontal_speed)
 
-func player_shoot(delta : float):
+func player_shoot(_delta : float):
 	var direction = input_movement()
 		
 	if direction != 0 and Input.is_action_just_pressed('shoot'):
@@ -109,6 +109,13 @@ func player_animations():
 	elif current_state == State.Shoot:
 		animated_sprite_2d.play('run_shoot')
 
+func player_death():
+	var player_death_effect_instance = player_death_effect.instantiate() as Node2D
+	player_death_effect_instance.global_position = global_position
+	get_parent().add_child(player_death_effect_instance)
+	queue_free()
+
+
 func input_movement():
 	var direction : float = Input.get_axis("move_left", "move_right")
 	
@@ -120,3 +127,6 @@ func _on_hurt_box_body_entered(body: Node2D) -> void:
 		print("Enemy Entered: ", body.damage_amount)
 		hit_animation_player.play("hit")
 		HealthManager.decrease_health(body.damage_amount)
+
+	if HealthManager.current_health == 0:
+		player_death()
