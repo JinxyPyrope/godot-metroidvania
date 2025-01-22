8698d2001e6e601cba72442a9beede15635b9eb2
JinxyPyrope <jinxypy@yahoo.com>
2025-01-22 10:40:33 -0500

    Add base foundation for screen sizes for testing

--- ui/settings_menu_screen.gd
+++ ui/settings_menu_screen.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@onready var window_mode_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/WindowModeOPtionButton
+@onready var window_mode_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/WindowModeOptionButton
 @onready var resolution_option_button: OptionButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ResolutionOptionButton
 
 
@@ -20,3 +20,14 @@ func _ready():
 	
 	for resolution in resolutions:
 		resolution_option_button.add_item(resolution)
+
+func _on_window_mode_option_button_item_selected(index: int) -> void:
+	var window_mode = window_modes.get(window_mode_option_button.get_item_text(index)) as int
+	print("window mode ", window_mode)
+
+func _on_resolution_option_button_item_selected(index: int) -> void:
+	var resolution = resolutions.get(resolution_option_button.get_item_text(index)) as Vector2i
+	print("resolution ", resolution)
+
+func _on_main_menu_button_pressed() -> void:
+	queue_free()
