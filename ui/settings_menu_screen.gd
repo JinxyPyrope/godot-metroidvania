c2a0ab6976cc6f22bfcf3c95212602af4a0b58ea
JinxyPyrope <jinxypy@yahoo.com>
2025-01-22 10:21:51 -0500

    making resolution list

--- ui/settings_menu_screen.gd
+++ ui/settings_menu_screen.gd
@@ -5,4 +5,12 @@ extends CanvasLayer
 
 
 var window_nodes : Dictionary = {"Fullscreen" : DisplayServer.WINDOW_MODE_FULLSCREEN,
-}
+								"Window" : DisplayServer.WINDOW_MODE_WINDOWED,
+								"Window Maximized" : DisplayServer.WINDOW_MODE_MAXIMIZED}
+								
+var resolutions : Dictionary = {"320x180" : Vector2i(320, 180),
+								"480x270" : Vector2i(480, 270),
+								"640x360" : Vector2i(640, 360),
+								"854x480" : Vector2i(640, 360),
+								"1280x720" : Vector2i(1280, 720)}
+									
