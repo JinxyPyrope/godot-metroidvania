eb0f3c038a01182b4e1cb51c15625f1b86479666
JinxyPyrope <jinxypy@yahoo.com>
2025-01-22 10:59:18 -0500

    Setting up the window function mode

---
+++ scripts/settings_manager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+var settings_data : SettingsDataResource
+
+#This setting creates a path for saving the settings n matter the device
+var save_settings_path = "user://game.data/"
+
+#This is the name of th efile we save i nthe folder location above
+var save_file_name = "settings_data.tres"
+
+
+func load_settings():
+	if !DirAccess.dir_exists_absolute(save_settings_path):
+		DirAccess.make_dir_absolute(save_settings_path)
+		
+	if ResourceLoader.exists(save_settings_path) :
+		settings_data = ResourceLoader.load(save_settings_path + save_file_name)
+	
+	if settings_data == null:
+		settings_data = SettingsDataResource.new()
+	
+	if settings_data != null:
+		set_window_mode(settings_data.window_mode, settings_data.window_mode_index)
+
+
+func set_window_mode(window_mode : int, window_mode_index : int):
+	match window_mode:
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.WINDOW_MODE_MAXIMIZED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+		_:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	settings_data.window_mode = window_mode
+	settings_data.window_mode_index = window_mode_index
