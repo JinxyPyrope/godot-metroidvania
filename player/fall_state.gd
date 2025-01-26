57c0b46c47359b1acc6587bdd1a230279e632460
JinxyPyrope <jinxypy@yahoo.com>
2025-01-26 14:30:42 -0500

    Make a fall state that transitions back to idle state

--- player/fall_state.gd
+++ player/fall_state.gd
@@ -3,15 +3,22 @@ extends NodeState
 @export var character_body_2d : CharacterBody2D
 @export var animated_sprite_2d : AnimatedSprite2D
 
+const GRAVITY : int = 700
+
 func on_process(delta : float):
 	pass
 	
 func on_physics_process(delta : float):
-	if !character_body_2d.is_on_floor():
-		transition.emit("Fall")
+	character_body_2d.velocity.y += GRAVITY * delta
 	
+	character_body_2d.move_and_slide()
+	
+	
+	#idle State transition
+	if character_body_2d.is_on_floor():
+		transition.emit("Idle")
 func enter():
-	animated_sprite_2d.play("idle")
+	animated_sprite_2d.play("fall")
 	
 func exit():
 	animated_sprite_2d.stop()
